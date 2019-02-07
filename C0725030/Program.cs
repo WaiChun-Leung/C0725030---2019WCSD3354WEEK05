a0cba378e5ef5dc4b0453d55c69e70dda7dce0c9
WaiChun-Leung <waichun.leung29@gmail.com>
2019-02-07 14:06:48 -0500

    205 PM COMMIT

--- C0725030/Program.cs
+++ C0725030/Program.cs
@@ -10,6 +10,55 @@ namespace C0725030
     {
         static void Main(string[] args)
         {
+            //WEEK 5
+            //Console.WriteLine("Hello World!");
+
+            //Question 2-1 (create object classes)
+            var a = new TestOuestion2();
+            a.PlayingWithForLoops();
+
         }
+    }
+    class TestOuestion2
+    {
+        public int myFavoriteVariable = 0;
+        public void PlayingWithForLoops()
+        {
+            //Question 2-1 (it will loop forever)
+            //for ( ; ; )
+            //{
+            //    Console.WriteLine("I am in the for loop");
+            //}
+
+            //Question 2-2
+            //Write a For Loop to Add 10 Numbers
+            //for ( ; false ; )     //HOTKEY is double click Tab
+            //    //false will run, but won't show anything
+            //    //TRUE infinite loop
+            //    //varibale initialize + check the condition + increasement
+            //{
+            //    Console.WriteLine("I am in the for loop");
+            //}
+
+            //Question 2-3 will run for 10 times
+            for (; MyMethod();) 
+            {
+                if (myFavoriteVariable > 10)
+                {
+                    Console.WriteLine("I am so out of here");
+                    break;
+                }
+                Console.WriteLine("Oh no I have to go through this stupid loop again... ");
+            }
+        }
+        public bool MyMethod()
+        {
+            myFavoriteVariable++;
+            return true;
+        }
+        //public bool MyMethod() //is infinite loop
+        //{
+        //    return true;
+        //}
     }
 }
